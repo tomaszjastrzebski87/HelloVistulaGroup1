8a5735d28afb57de3e7b78729e6eda7c2f5d4593
tomaszjastrzebski87 <tjastrz1@stu.vistula.edu.pl>
2024-12-28 15:33:22 +0100

    Refactor PalindromeChecker.java

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -5,9 +5,9 @@ public class PalindromeChecker {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter a word to check: ");
         String word = scanner.nextLine().toLowerCase();
-        StringBuilder reversedWord = new StringBuilder(word);
+        StringBuilder reversedWord = new StringBuilder(word).reverse();
 
-        if (word.contentEquals(reversedWord.reverse())) {
+        if (word.contentEquals(reversedWord)) {
             System.out.println("Entered word is a palindrome.");
         } else {
             System.out.println("Entered word is not a palindrome.");
